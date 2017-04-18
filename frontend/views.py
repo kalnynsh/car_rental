511a8edb0679def65be041cebd832725d4c52d04
kalnynsh <kda869@yandex.ru>
2017-04-18 17:55:23 +0300

    In frontend/views create class CarDetailsView(DetailView).

--- frontend/views.py
+++ frontend/views.py
@@ -2,6 +2,31 @@ from django.views.generic import CreateView
 from django.views.generic import DetailView
 from django.views.generic import TemplateView
 
+from frontend import Car
+from frontend import Booking
+
 
 class HomeView(TemplateView):
-    template_name = ''
+    template_name = 'frontend/home.html'
+
+    def get_context_data(self, **kwargs):
+        ctx = super(HomeView, self).get_context_data(**kwargs)
+
+        cars = Car.objects.filter(is_available=True)
+        ctx['cars'] = cars
+
+        return ctx
+
+
+class CarDetailsView(DetailView):
+    template_name = 'frontend/car_details.html'
+    model = Car
+
+    def get_context_data(self, **kwargs):
+        ctx = super(CarDetailsView, self).get_context_data(**kwargs)
+        ctx['booking_success'] = 'booking-success' in self.request.GET
+
+        return ctx
+
+
+
