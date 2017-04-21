ead23fb351addb9e10cb8f99644442347634fc93
kalnynsh <kda869@yandex.ru>
2017-04-21 19:21:11 +0300

    In frontend/admin create class BookingModelAdmin(admin.ModelAdmin) with list_display = [...]

--- frontend/admin.py
+++ frontend/admin.py
@@ -5,3 +5,12 @@ from frontend.models import Booking
 
 admin.site.register(Car)
 admin.site.register(Booking)
+
+
+class BookingModelAdmin(admin.ModelAdmin):
+    list_display = [
+        'customer_name',
+        'booking_start_date',
+        'booking_end_date',
+        'is_approved',
+    ]
