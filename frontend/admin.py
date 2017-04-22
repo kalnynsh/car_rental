c182161089bec21cdf380a091eaabfd59632a8ab
kalnynsh <kda869@yandex.ru>
2017-04-22 18:22:52 +0300

    In frontend/admin in class BookingModelAdmin add actions = ['email_customer'] with class method email_customer.

--- frontend/admin.py
+++ frontend/admin.py
@@ -17,6 +17,26 @@ class BookingModelAdmin(admin.ModelAdmin):
 
     list_editable = ['is_approved']
 
+    actions = ['email_customers']
+
+    def email_customers(self, request, queryset):
+        for booking in queryset:
+            if booking.is_approved:
+                email_body = """Dear {},
+        We are pleased to inform you that your booking has been approved.
+        Thanks
+                """.format(booking.customer_name)
+            else:
+                email_body = """Dear {},
+        Unfortunately we do not have the capacity right now to accept your
+        booking.
+        Thanks
+        """.format(booking.customer_name)
+
+                print(email_body)
+
+    email_customers.short_description = "" \
+        "Send email customer with approved or not message"
 
 admin.site.register(Car)
 admin.site.register(Booking, BookingModelAdmin)
